3b064695ee0201e847e8a8ab8939a18e2dbe5f1a
pxdeoleo <pdeoleo@ipexdr.com>
2021-08-19 17:58:26 -0400

    Ultimas correcciones

--- ExamenFinal/Program.vb
+++ ExamenFinal/Program.vb
@@ -42,14 +42,18 @@ Module Program
         If x.Any Then
 
             For Each item As Double In x
-                suma += Math.Pow(item - x.Average, 2)
+                suma += (item - x.Average) ^ 2
             Next
-            'sum es la suma de todos los datos.
-            'function(num) es para interactuar con los datos del arreglo
-            'pow es para elevar a una cantidad en este caso 2
-            'average es el promedio
+            '+= es la forma en la que pondemos incrementar una misma variable con su valor mas lo que se le quiere agregar
+            'ejemplo suma = suma + Math.Pow(item - x.Average, 2)
+            'eso es igual que yo poner suma += Math.Pow(item - x.Average, 2)
+            'Entonces el for each recorre el arreglo que recibio la funcion
+            'item es el indice por el cual va el arreglo osea, numbers(0) es la primera posicion del arreglo, entonces numbers(1) es la segunda y asi
+            'entonces el coge la posicion y le resta el promedio del arreglo, el promedio es la suma de todos los elemntos entre la cantidad de los mismo elementos
+            'ejemplo {2, 2, 2, 2, 2}, entonces el promedio de eso es la suma de todos esos numero entre la cantidad de elementos que sumaste (2+2+2+2+2)/  
             'entonces al numero se le resta el promedio y se eleva al cuadrado
-            '
+            'POW es para elevar a una cantidad en este caso 2
+
             resultado = Math.Sqrt((suma) / (x.Length - 1))
             'SQRT es para sacar la raiz del resultado de la suma final que calculamos arriba entre el tama;o de nuestro arreglo
             Return resultado
@@ -58,11 +62,9 @@ Module Program
     End Function
 
     Sub Main(args As String())
-        Dim continuar As Boolean = True
-
         Console.Title = "The Hollow"
 
-        Console.WriteLine("Mientras nuestros 3 amigos despertaron de un largo sueño, encerrados en una")
+        Console.WriteLine("Mientras nuestros 3 amigos despertaron de un largo sue�o, encerrados en una")
         Console.WriteLine("habitación, estaba preguntándose, ¿quiénes eran?, de pronto, uno de ellos encontró")
         Console.WriteLine("un papel dentro del pantalón con un nombre, luego los demás hicieron lo mismo." + Environment.NewLine)
 
@@ -84,6 +86,8 @@ Module Program
 
             intentos += 1
 
+            Console.WriteLine($"Intento {intentos}: ")
+
             For h As Integer = 0 To 2 Step 1
                 'Bucle para asignar los nombres aleatoriamente.
                 Dim player As String
@@ -96,10 +100,11 @@ Module Program
                 If nombres.GetRange(3, 2).Contains(player) Then
                     mujer = player
                 End If
-                Console.WriteLine($"Intento: {intentos} | Nombre: {player}")
-            Next
-            Console.WriteLine("--------------")
 
+                Console.Write(player + ", ")
+            Next
+
+            Console.WriteLine()
 
             If hombres.Count = 2 And Not String.IsNullOrEmpty(mujer) Then
                 'Si hay dos hombres y la variable Mujer tiene valor, entonces se rompe
@@ -115,7 +120,6 @@ Module Program
         End While
 
         Console.WriteLine($"Los nombres de nuestros héroes son {hombres(0)}, {mujer} y {hombres(1)}" + Environment.NewLine)
-        Console.ReadKey()
 
 #End Region
 
@@ -124,23 +128,23 @@ Module Program
         intentos = 0
 
         Console.WriteLine("Dentro de la habitación hay una máquina de escribir, la cual le va a permitir salir de")
-        Console.WriteLine("la habitación escribiendo con ella la palabra mágica, la cual debe ser una palabra")
-        Console.WriteLine("Palíndroma." + Environment.NewLine)
+        Console.WriteLine("la habitaci�n escribiendo con ella la palabra m�gica, la cual debe ser una palabra")
+        Console.WriteLine("Pal�ndroma." + Environment.NewLine)
 
         'Bucle de intentos
         While True
             Console.Write("Ingrese la palabra magica: ")
-            palabra = Console.ReadLine().ToLower()
-
-            If esPalindroma(palabra) Then
+            palabra = Console.ReadLine().ToLower() ' sometemos
+            If esPalindroma(palabra) And palabra.Length > 1 Then
+                Console.WriteLine("Bien hecho. Palabra magica aceptada." + Environment.NewLine)
                 Exit While
             Else
                 If intentos = 5 Then
                     Console.WriteLine("THE HOLLOW CANNOT BE INITIALIZED, GAME OVER.")
                     GameOver()
                 End If
-                Console.WriteLine("Por favor intentelo de nuevo.")
                 intentos += 1
+                Console.WriteLine("Por favor intentelo de nuevo. Le Restan {0} intentos ", 5 - intentos)
             End If
 
         End While
@@ -149,10 +153,10 @@ Module Program
 
         Console.WriteLine($"Luego de que nuestros amigos {hombres(0)}, {mujer} y {hombres(1)} son")
         Console.WriteLine("sacados de la habitación, aparecieron en un camino helado y desconocido. En este")
-        Console.WriteLine("camino, encontraron un mapa, con la ubicación secreta de la llave que los llevaría a")
+        Console.WriteLine("camino, encontraron un mapa, con la ubicaci�n secreta de la llave que los llevar�a a")
         Console.WriteLine("su casa." + Environment.NewLine)
         Console.WriteLine("El mapa estaba compuesto de 3 partes, las cuales se hacen visible, cada vez que")
-        Console.WriteLine("puedan vencer un desafío." + Environment.NewLine)
+        Console.WriteLine("puedan vencer un desaf�o." + Environment.NewLine)
 
 #Region "Desafio no. 1"
         Console.WriteLine("Para poder hacer visible la segunda parte del mapa, deben poder llegar a la cima de")
@@ -163,12 +167,13 @@ Module Program
         Dim x(1) As Integer
         Dim suma, sumaCuadrado, promedio, total As Integer
         Dim tiempo, velocidad As Integer
-        'Dim A, B, C, D, E As Integer
         Dim i As Integer = 0
         Dim numbers(4) As Double
 
+        'La primera evaluacion es 2 > 0
         While x.Length > i
-            Console.WriteLine("Introduzca el {0}er numero", i + 1)
+            ' i + 1 es para que el usuario vea "1 numero" en vez de "0 numero"
+            Console.WriteLine("Introduzca el {0}º numero", i + 1)
             Try
                 x(i) = Console.ReadLine()
                 Console.WriteLine()
@@ -180,38 +185,44 @@ Module Program
 
         End While
 
-        Select Case x.Any
-            Case x(0) > x(1)
-                Console.WriteLine("MAYOR")
+        Select Case x.Any 'Verifica si el arreglo esta vacio 
+            Case x(0) > x(1) 'Verificamos que el primer numero introducido sea mayor que el segundo
                 suma = x(0) + x(1)
 
                 If (suma Mod x(0)) = 0 Or (suma Mod x(1)) = 0 Then
                     Console.WriteLine("La suma tiene una division exacta con alguno de los numeros")  'NO SE QUE HACER AQUI SI NO SE PUEDE
+                    Console.WriteLine()
                     i = 0
-                    While numbers.Length > i
-                        Console.WriteLine("Introduzca el {0}er numero", i + 1)
+                    While numbers.Length > i 'Un While que coge 5 numero para cacular la desviacion estandar
+                        Console.WriteLine("Introduzca el {0}º numero", i + 1)
                         Try
                             numbers(i) = Console.ReadLine()
                             Console.WriteLine()
                             i += 1
                         Catch ex As Exception
-                            Console.WriteLine("DEBE INTRODUCIR EL TIEMPO")
+                            Console.WriteLine("DEBE INTRODUCIR UN NUMERO")
                             Console.WriteLine()
                         End Try
                     End While
 
-                    '(C > ((A+B+D+E)/4)) OR (E = ((A + B) * D))
-                    If (numbers(2) > ((numbers(0) + numbers(1) + numbers(3) + numbers(4)) / 4)) Or (numbers(4) = ((numbers(0) + numbers(1)) * numbers(3))) Then
-                        Console.WriteLine("Es la distancia para recorrer para llegar a la cima de la montaña: {0}", Math.Round(DesvEst(numbers) * 10, 2))
+                    'Calcula que el el tercer numero introducido sea mayor que la suma de los otros y dividio entre 4
+                    'O el ultimo numero debe ser igual a la suma del primero y segundo, el resultado de eso multiplicado por el cuarto numero introducido
+                    If (numbers(2) > ((numbers(0) + numbers(1) + numbers(3) + numbers(4)) / 4)) Then
+                        '(C > ((A+B+D+E)/4)) 
+                        Console.WriteLine("La distancia para recorrer para llegar a la cima de la montaña: {0} metros", Math.Round(DesvEst(numbers) * 10, 2))
+                    ElseIf (numbers(4) = ((numbers(0) + numbers(1)) * numbers(3))) Then
+                        '(E = ((A + B) * D))
+                        Console.WriteLine("La distancia para recorrer para llegar a la cima de la montaña: {0} metros", Math.Round(DesvEst(numbers) * 10, 2))
                     Else
-                        Console.WriteLine("OurFriends are already frozen, this is a GAMEOVER")
+                        'Se acaba el juego si la cantidad de arriba no tiene una solucion
+                        Console.WriteLine("Our Friends are already frozen, this is a GAMEOVER")
                         GameOver()
                     End If
 
-                Else
-                    sumaCuadrado = x(0) ^ 2 + x(1) ^ 2
-                    promedio = suma / 2
-                    total = suma + sumaCuadrado + promedio
+                Else 'Si ninguno de los numeros divide a la suma exactamente hacemos esto a continuacion
+                    sumaCuadrado = x(0) ^ 2 + x(1) ^ 2 'La Suma de cuadrado es elevar al cuadrado ambos numeros introducidos y sumarlo
+                    promedio = suma / 2  'El promedio es la suma de todos los numeros y dividido entra la cantidad que se sumaron
+                    total = suma + sumaCuadrado + promedio ' La suma total es la suma de ambos numeros, sumacuadrado y el promedio
 
                     If total > 100 Then
                         While True
@@ -241,6 +252,10 @@ Module Program
                         End While
 
                         Console.WriteLine("La distancia es {0} KM", Math.Round((velocidad * (tiempo / 60)), 2))
+                        'El tiempo se divide entre 60 porque la hay que convertir los minutos en la cantidad de horas que es
+                        'La velocidad se multiplica por el tiempo en 
+                        'd = v * t
+                        'Round es para rendondear a dos posiciones los decimales 
 
                     ElseIf total < 100 Then
                         Console.WriteLine("GAME OVER")
@@ -249,8 +264,8 @@ Module Program
 
                 End If
 
-            Case x(1) > x(0)
-                Console.WriteLine("MENOR")
+            Case x(1) > x(0) ' Esta es una condicion que si el segundo numero es mayor que el primer numero introducido
+                'Entonces coge 5 numero para calcular la desviacion estandar
                 i = 0
                 While numbers.Length > i
                     Console.WriteLine("Introduzca el numero {0}", i + 1)
@@ -264,11 +279,16 @@ Module Program
                     End Try
                 End While
 
-                '(C > ((A+B+D+E)/4)) OR (E = ((A + B) * D))
-                If (numbers(2) > ((numbers(0) + numbers(1) + numbers(3) + numbers(4)) / 4)) Or (numbers(4) = ((numbers(0) + numbers(1)) * numbers(3))) Then
-                    Console.WriteLine("Es la distancia para recorrer para llegar a la cima de la montaña: {0}", Math.Round(DesvEst(numbers) * 10, 2))
+                'Calcula que el el tercer numero introducido sea mayor que la suma de los otros y dividio entre 4
+                'O el ultimo numero debe ser igual a la suma del primero y segundo, el resultado de eso multiplicado por el cuarto numero introducido
+                If (numbers(2) > ((numbers(0) + numbers(1) + numbers(3) + numbers(4)) / 4)) Then
+                    '(C > ((A+B+D+E)/4))
+                    Console.WriteLine("Es la distancia para recorrer para llegar a la cima de la montaña: {0} metros", Math.Round(DesvEst(numbers) * 10, 2))
+                ElseIf (numbers(4) = ((numbers(0) + numbers(1)) * numbers(3))) Then
+                    '(E = ((A + B) * D))
+                    Console.WriteLine("Es la distancia para recorrer para llegar a la cima de la montaña: {0} metros", Math.Round(DesvEst(numbers) * 10, 2))
                 Else
-                    Console.WriteLine("OurFriends are already frozen, this Is a GAMEOVER�")
+                    Console.WriteLine("OurFriends are already frozen, this is a GAMEOVER")
                     GameOver()
                 End If
             Case Else
@@ -288,7 +308,7 @@ Module Program
         Console.WriteLine("gran ARES, el Dios de la Guerra injusta. Les va a proporcionar la lanza si resuelven este acetijo." + Environment.NewLine)
 
         Console.WriteLine("Cada  digito  ha  sido  reemplazado  por  una  letra.  Cada  letra  representa  el  mismo")
-        Console.WriteLine("número allí donde aparece. Los dígitos son 0, 4 y 8 (A, B y C respectivamente). " + Environment.NewLine)
+        Console.WriteLine("número allá donde aparece. Los d�gitos son 0, 4 y 8. " + Environment.NewLine)
 
         Console.WriteLine("
         A   C	A
@@ -363,7 +383,7 @@ Module Program
             letra2 = Console.ReadKey.KeyChar
             Console.WriteLine()
 
-            If Char.IsLetter(letra1) Then
+            If Char.IsLetter(letra2) Then
                 Exit While
             Else
                 Console.WriteLine("Por favor ingrese una letra que pertenezca al alfabeto.")
@@ -371,7 +391,8 @@ Module Program
         End While
 
         ' Se convierten la letra a Byte (Su representacion ASCII) y posteriormente a String
-
+        'k -> "107"
+        'm -> "109"
         letra1Bin = Convert.ToByte(letra1).ToString
         letra2Bin = Convert.ToByte(letra2).ToString
 
@@ -388,19 +409,21 @@ Module Program
             End If
         End If
 
-        Console.WriteLine($"{luchador} sérá quien luche contra el dragón.")
-        Console.WriteLine("Para vencer al dragón con la lanza, el luchador debe encontrar su punto débil.")
+        Console.WriteLine($"{luchador} s�r� quien luche contra el dragón.")
+        Console.WriteLine("Para vencer al drag�n con la lanza, el luchador debe encontrar su punto débil.")
 
         Dim x1, x2, y1, y2 As Integer
         Dim Distancia As Integer
 
         While True
-            Console.WriteLine($"Ingrese la coordenada X de {luchador}: ")
+            Console.WriteLine($"Ingrese la coordenada X de {luchador}: (Rango entre 500 y 1000)")
 
             'Se verifica que el dato que ingrese el usuario sea un numero
             'Y que este entre 500 y 1000
             If Integer.TryParse(Console.ReadLine, x1) Then
-                If x1 < 500 Or x1 > 1000 Then
+                If x1 >= 500 And x1 <= 1000 Then
+                    Exit While
+                Else
                     Console.WriteLine("El numero debe estar entre 500 y 1000. Intentelo de nuevo")
                     Continue While
                 End If
@@ -410,7 +433,7 @@ Module Program
         End While
 
         While True
-            Console.WriteLine($"Ingrese la coordenada Y de {luchador}: ")
+            Console.WriteLine($"Ingrese la coordenada Y de {luchador}:  (Rango entre 500 y 1000)")
 
             If Integer.TryParse(Console.ReadLine, x2) Then
                 'Ademas de las validaciones anteriores, se comprueba que el numero que se ingrese
@@ -420,7 +443,9 @@ Module Program
                     Continue While
                 End If
 
-                If x1 < 500 Or x1 > 1000 Then
+                If x2 >= 500 And x2 <= 1000 Then
+                    Exit While
+                Else
                     Console.WriteLine("El numero debe estar entre 500 y 1000. Intentelo de nuevo")
                     Continue While
                 End If
@@ -428,7 +453,7 @@ Module Program
         End While
 
         While True
-            Console.WriteLine("Ingrese la coordenada X del dragon: ")
+            Console.WriteLine("Ingrese la coordenada X del dragon: (Rango entre 500 y 1000)")
 
             If Integer.TryParse(Console.ReadLine, y1) Then
                 If y1 = x1 Or y1 = x2 Then
@@ -436,7 +461,9 @@ Module Program
                     Continue While
                 End If
 
-                If x1 < 500 Or x1 > 1000 Then
+                If y1 >= 500 And y1 <= 1000 Then
+                    Exit While
+                Else
                     Console.WriteLine("El numero debe estar entre 500 y 1000. Intentelo de nuevo")
                     Continue While
                 End If
@@ -444,7 +471,7 @@ Module Program
         End While
 
         While True
-            Console.WriteLine("Ingrese la coordenada Y del dragon: ")
+            Console.WriteLine("Ingrese la coordenada Y del dragon: (Rango entre 500 y 1000)")
 
             If Integer.TryParse(Console.ReadLine, y2) Then
                 If y2 = y1 Or y2 = x1 Or y2 = x2 Then
@@ -452,7 +479,9 @@ Module Program
                     Continue While
                 End If
 
-                If x1 < 500 Or x1 > 1000 Then
+                If y2 >= 500 And y2 <= 1000 Then
+                    Exit While
+                Else
                     Console.WriteLine("El numero debe estar entre 500 y 1000. Intentelo de nuevo")
                     Continue While
                 End If
@@ -462,7 +491,7 @@ Module Program
 
         Distancia = Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2)
 
-        Console.WriteLine($"El dragón está a {Distancia} metros." + Environment.NewLine)
+        Console.WriteLine($"El drag�n est� a {Distancia} metros." + Environment.NewLine)
         Console.WriteLine($"{luchador} arroja la lanza con todas sus fuerzas, impactando al dragón, que cae herido de gravedad." + Environment.NewLine)
         Console.WriteLine("Hay un monstruo con mucho más poder que él, y es mejor no enfrentarlo, para hacer")
         Console.WriteLine("esto y poder subir la montaña, deben pronunciar su nombre tres veces, uno cada")
@@ -522,11 +551,11 @@ Module Program
 #End Region
 
         Console.WriteLine($"Gritaron {hombres(0)}, {mujer} y {hombres(1)}.")
-        Console.WriteLine("Inmediatamente son teletransportados a la cima de la montaña.")
-        Console.WriteLine("Habiendo cumplido los 3 desafíos, la llave es")
+        Console.WriteLine("Inmediatamente son teletransportados a la cima de la monta�a.")
+        Console.WriteLine("Habiendo cumplido los 3 desaf�os, la llave es")
         Console.WriteLine("entregada a ellos por el venger, que admitiendo")
         Console.WriteLine("su derrota les permite continuar su camino hacia")
-        Console.WriteLine("su hogar entregándoles la llave del gran portón." + Environment.NewLine)
+        Console.WriteLine("su hogar entreg�ndoles la llave del gran portón." + Environment.NewLine)
 
         Console.WriteLine("Fin del juego. Presione cualquier tecla para concluir.")
         Console.ReadKey()
